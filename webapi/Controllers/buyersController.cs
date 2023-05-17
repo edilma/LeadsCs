d6f682937b3bfd9ef9a56834f78035f1adffba64
Edilma Riano <edilmaria@hotmail.com>
2023-05-16 21:25:29 -0400

    ADDED buyers controller

---
+++ webapi/Controllers/buyersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapi.Models;
+
+namespace webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class buyersController: ControllerBase
+    {
+
+        //Get all buyers from database   
+        [HttpGet]
+        public IEnumerable<Buyer> Get()
+        {
+            return new List<Buyer>
+            {
+                new Buyer
+                {
+                    Id = 1,
+                    companyName = "Boca Code",
+                    contactName = "Joe Doe",
+                    email = ""
+                }
+            };
+        }
+    }
+}
