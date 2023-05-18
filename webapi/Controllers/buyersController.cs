97219cf6b85408acae089e8e0359412001ba98c5
Edilma Riano <edilmaria@hotmail.com>
2023-05-18 11:50:27 -0400

    TESTED and added more buyer functionality

--- webapi/Controllers/buyersController.cs
+++ webapi/Controllers/buyersController.cs
@@ -23,5 +23,34 @@ namespace webapi.Controllers
                 }
             };
         }
+
+        //Get buyer by id from the database
+        [HttpGet("{id}")]
+        public Buyer GetBuyerById(int id)
+        {
+            return new Buyer
+            {
+                Id = 1,
+                companyName = "Boca Code",
+                contactName = "Joe Doe",
+                email = "",
+                phone_number = "555-555-5555",
+                city = "Boca Raton",
+                zip = "33431"
+            };
+        }
+
+        // create a new buyer  
+        [HttpPost]
+        public async Task<IResult> AddBuyer([FromBody] Lead lead)
+        {
+            // use the "lead" variable to write to the database
+            //todo
+
+
+            return Results.Created("/Buyer", new Response { Message = "Hello" });
+        }
+
+
     }
 }
