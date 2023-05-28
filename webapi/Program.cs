5431870855613bcc4edb65680d48bd4807f09251
Edilma Riano <edilmaria@hotmail.com>
2023-05-27 21:39:11 -0400

    CONFIGURED connection to database

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 using webapi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,10 +15,10 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 // use a service serializer for Json
 builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
-
 //Dependency injection for all controllers that ask for LDSContext
 builder.Services.AddDbContext<LDSContext>(options =>
-   options.UseSqlServer(builder.Configuration.GetConnectionString("LDSsystem")));
+   options.UseSqlServer(builder.Configuration.GetConnectionString("LDSsystem"))
+   );
 
 var app = builder.Build();
 
